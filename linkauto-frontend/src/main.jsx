7e37453567add7c13a55e71e924ae55559482bc2
gabrildnsilva <gabrieldnsilva@gmail.com>
2026-04-18 01:30:34 -0300

    chore: Updated package.json / lock + main.jsx wiring modules

--- linkauto-frontend/src/main.jsx
+++ linkauto-frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import AppRouter from "./app/router.jsx";
 import "./index.css";
+import { SessionProvider } from "./state/sessionStore.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<SessionProvider>
+			<AppRouter />
+		</SessionProvider>
+	</React.StrictMode>,
 );
